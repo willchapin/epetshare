fdab2ff8d68368bd75e369a4d4953d393dda9fc1
willchapin <wrchapin@gmail.com>
2014-01-26 18:55:59 -0500

    add message model and spec

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,21 @@
 class Message < ActiveRecord::Base
 
+  attr_accessible :content, :title, :recipient
+
+  # belong_to :user and and not sender!
+  
   belongs_to :sender, class_name: 'User'
   belongs_to :recipient, class_name: 'User'
   
+  validates_presence_of :content, :title, :sender, :recipient
+  
+  validates_length_of :title, maximum: 100
+  validates_length_of :content, maximum: 2500
+  validate :sender_is_not_recipient
+  
+  private
+
+  def sender_is_not_recipient
+    errors.add(:recipient, "can't be sender") if sender == recipient
+  end  
 end
---
+++ spec/models/message_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe Message do
+
+  let(:user) { User.create(firstname: 'tan',
+                           lastname: 'tam',
+                           email: 'tan@tam.tom') }
+  
+  it { should respond_to(:content) }
+  it { should respond_to(:title) }
+  it { should respond_to(:sender) }  
+  it { should respond_to(:recipient) }
+
+  it { should allow_mass_assignment_of(:content) }
+  it { should allow_mass_assignment_of(:title) }
+  it { should allow_mass_assignment_of(:recipient) }
+
+  it { should validate_presence_of(:content) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:sender) }
+  it { should validate_presence_of(:recipient) }
+
+  it "checks that the sender is not the recipient" do
+    message = user.sent_messages.create({ content: "I'm bad",
+                                                  title: "So bad",
+                                                  recipient: user })
+    expect(message).to_not be_valid
+  end
+
+  it { should ensure_length_of(:title).is_at_most(100) }
+  it { should ensure_length_of(:content).is_at_most(2500) }
+
+  it { should belong_to(:sender) }
+  it { should belong_to(:recipient) }
+  
+end
