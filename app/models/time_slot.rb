455414eda8fab95825b57bfc945f5b6a7d875d7f
willchapin <wrchapin@gmail.com>
2014-01-26 00:00:13 -0500

    add time_slot model and spec

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -1,5 +1,15 @@
 class TimeSlot < ActiveRecord::Base
 
   belongs_to :user
-  
+
+  attr_accessible :user, :start_time, :end_time, :time_type
+
+  validates_presence_of :user, :start_time, :end_time, :time_type
+
+  private
+
+  def range
+  end
+
+
 end
---
+++ spec/models/time_slot_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe TimeSlot do
+
+  before(:all) do
+    @user = User.create(firstname: 'tan',
+                        lastname: 'tam',
+                        email: 'tan@tam.tom')    
+    @pet = Pet.create(name: 'bubba the cloud',
+                      user: @user,
+                      species: 'dog')
+  end
+  
+  it { should belong_to(:user) }
+  
+  it { should respond_to(:user) }
+  it { should respond_to(:start_time) }
+  it { should respond_to(:end_time) }
+  it { should respond_to(:time_type) }
+
+  # lookup zip based on state/address?
+  
+  it { should allow_mass_assignment_of(:user) }
+  it { should allow_mass_assignment_of(:start_time) }
+  it { should allow_mass_assignment_of(:end_time) }
+  it { should allow_mass_assignment_of(:time_type) }
+
+  it { should validate_presence_of(:user) }
+  it { should validate_presence_of(:start_time) }
+  it { should validate_presence_of(:end_time) }
+  it { should validate_presence_of(:time_type) }
+
+  it 'validates there is no time_slot overlap' do
+  end
+
+  
+end
