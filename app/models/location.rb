69990b21fadc9a5505b215ebe1b8b0f13212e417
willchapin <wrchapin@gmail.com>
2014-01-25 23:55:48 -0500

    add location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,13 @@
 class Location < ActiveRecord::Base
 
   belongs_to :user
+
+  attr_accessible :user, :address, :city, :state, :zip
+
+  validates_presence_of :user, :address, :city, :state, :zip
+  validates_length_of :address, maximum: 50
+  validates_length_of :city, maximum: 40
+  validates_length_of :state, maximum: 12
+  validates_length_of :zip, is: 5
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140123200739) do
+ActiveRecord::Schema.define(:version => 20140124165953) do
 
   create_table "locations", :force => true do |t|
     t.integer  "user_id",    :null => false
