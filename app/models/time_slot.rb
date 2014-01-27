a4682026a998240fcc2e6d2117b909efc034b626
willchapin <wrchapin@gmail.com>
2014-01-26 23:07:46 -0500

    broken :(

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -2,14 +2,26 @@ class TimeSlot < ActiveRecord::Base
 
   belongs_to :user
 
-  attr_accessible :user, :start_time, :end_time, :time_type
+  attr_accessible :start_time, :end_time, :time_type
 
   validates_presence_of :user, :start_time, :end_time, :time_type
 
+  validate :no_overlap, :start_before_end
+
   private
 
-  def range
+  def start_before_end
+    errors.add(:start_time, "sad sad tears on my sad face") if start_time < end_time
   end
 
-
+  # add index on user/time_type
+  def no_overlap
+    self.class.where(user_id: user.id, time_type: time_type).each do |other_time|
+      errors.add(:start_time, "i'm a sad error") if overlaps?(other_time)
+    end
+  end
+  
+  def overlaps?(other_time)
+    start_time <= other_time.end_time && end_time >= other_time.start_time
+  end
 end
--- spec/models/time_slot_spec.rb
+++ spec/models/time_slot_spec.rb
@@ -6,10 +6,16 @@ describe TimeSlot do
     @user = User.create(firstname: 'tan',
                         lastname: 'tam',
                         email: 'tan@tam.tom',
-                        password: 'sigh')    
-    @pet = Pet.create(name: 'bubba the cloud',
-                      user: @user,
-                      species: 'dog')
+                        password: 'sigh')
+    
+    @time_slot = @user.time_slots.create(start_time: 3.days.ago,
+                                         end_time: DateTime.current,
+                                         time_type: "sit")
+  end
+
+  after(:all) do
+    User.destroy_all
+    TimeSlot.destroy_all
   end
   
   it { should belong_to(:user) }
@@ -20,8 +26,7 @@ describe TimeSlot do
   it { should respond_to(:time_type) }
 
   # lookup zip based on state/address?
-  
-  it { should allow_mass_assignment_of(:user) }
+
   it { should allow_mass_assignment_of(:start_time) }
   it { should allow_mass_assignment_of(:end_time) }
   it { should allow_mass_assignment_of(:time_type) }
@@ -31,8 +36,15 @@ describe TimeSlot do
   it { should validate_presence_of(:end_time) }
   it { should validate_presence_of(:time_type) }
 
-  it 'validates there is no time_slot overlap' do
+  describe "validates that there is no overlap" do
+    it 'when start and end time are totally contained in a previous slot' do
+      @bad_time_slot = @user.time_slots.create(start_time: 2.days.ago,
+                                               end_time: 1.day.ago,
+                                               time_type: "sit")
+      puts "HEY!!! \n\n"
+      puts @bad_time_slot.errors.inspect
+      expect(@bad_time_slot).to_not be_valid
+    end
   end
-
   
 end
